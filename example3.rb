d8891555894dcd380afcc06b491c9b07024fe498
Taylor High <hightaylor@gmail.com>
2017-01-28 02:04:10 -0700

    Add third exercise and add comments to better understand each line of code

---
+++ example3.rb
@@ -0,0 +1,28 @@
+#This line will print the text.
+puts "I will now count my chickens:"
+# This line will print hens, while the hash and brackets tell the code to execute the calculation, rather than print what it's being fed.
+puts "Hens #{25 + 30 / 6}"
+#This line will do the same as described above.
+puts "Roosters #{100 - 25 * 3 % 4}"
+#This line will print the string.
+puts "Now I will count the eggs:"
+#This line will calculate the solution.
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#This line will print the string.
+puts "Is it true that 3 + 2 < 5 - 7?"
+#This line will calculate if 3 + 2 is less than 5 - 7.
+puts 3 + 2 < 5 - 7
+#This line will print the string, and the hash and curly brackets will execute the solution.
+puts "What is 3 + 2? #{3 + 2}"
+#Same as described as above.
+puts "What is 5 - 7? #{5 - 7}"
+#Print the string.
+puts "Oh, that's why it's false."
+#Print the string.
+puts "How about some more?"
+#print the string and return true/false based on equation.
+puts "Is it true? #{5 > -2}"
+#print the string and return true/false based on equation.
+puts "Is it greater or equal? #{5 >= -2}"
+#print the string and return true/false based on equation.
+puts "Is it less or equal? #{5 <= -2}"
